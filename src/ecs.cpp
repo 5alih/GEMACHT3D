d86c4950b72644d314918fb65dfb1286fed135de
Salih <mustafahakan9215@gmail.com>
2024-11-26 12:16:07 +0300

    Completed ECS

--- src/ecs.cpp
+++ src/ecs.cpp
@@ -12,27 +12,50 @@ std::vector<int> Entity::GetComponentTypes(){
 	return this->component_types;
 }
 
+int Entity::GetComponentType(int index){
+	return this->component_types[index];
+}
+
 std::vector<int> Entity::GetComponentIds(){
 	return this->component_ids;
 }
 
-	//find the component type by id
-	//clone the component to type's array
-	//save the newly generated component clone's id
-
-void Entity::RemoveComponent(int ComponentTypeId){
-	//remove component by id
-	return;
+int Entity::GetComponentId(int index){
+	return this->component_ids[index];
 }
 
-//_______________________________________________________________________________________________//
 
-// Initialize ECS
+void Entity::RemoveComponent(int componentTypeId) {
+    auto typeIt = std::find(component_types.begin(), component_types.end(), componentTypeId);
+    if (typeIt != component_types.end()) {
+        int index = std::distance(component_types.begin(), typeIt);
+        int componentInstanceId = component_ids[index];
+        if (ecs->componentArrays.find(componentTypeId) != ecs->componentArrays.end()) {
+            auto& componentArray = ecs->componentArrays[componentTypeId];
+
+            if ((size_t)componentInstanceId < componentArray.size()) {
+                componentArray[componentInstanceId] = nullptr; // Avoid erasing to prevent index changes
+            }
+        }
+        component_types.erase(typeIt);
+        component_ids.erase(component_ids.begin() + index);
+    }
+}
+
+//_______________________________________________________________ INITIALIZE ________________________________________________________________
+
 void ECS::Initialize() {
     entities.reserve(32);
+
+	this->RegisterComponentType<TransformComponent>(TRANSFORM_COMPONENT); // 0 is the type ID for TransformComponent
+	this->RegisterComponentType<HealthComponent>(HEALTH_COMPONENT);
+
+	this->AddSystem(std::make_shared<MovementSystem>());
+	this->AddSystem(std::make_shared<HealthSystem>());
 }
 
-// Create an entity and associate it with the ECS instance
+//___________________________________________________________________________________________________________________________________________
+
 Entity ECS::CreateEntity() {
     Entity entity(this);  // Pass a pointer to the current ECS instance
     entity.SetId(entityId++);
@@ -49,10 +72,16 @@ void ECS::DeleteEntity(int id) {
     }
 }
 
-void ECS::Update(){
-	//update components
+void ECS::Update() {
+    for (auto& system : systems) {
+        system->Update(*this);
+    }
 }
 
 void ECS::Shutdown(){
 	entities.clear();
+}
+
+void ECS::AddSystem(std::shared_ptr<System> system) {
+    systems.push_back(system);
 }
--- src/ecs.h
+++ src/ecs.h
@@ -5,44 +5,63 @@
 #include <memory>
 #include <unordered_map>
 #include <variant>
-#include <iostream>
+#include <bits/stdc++.h>
 #include "raylib.h"
 
-struct TransformComponent {
+//_______________________________________________________________ COMPONENTS ________________________________________________________________
+
+enum componentsEnum{TRANSFORM_COMPONENT, HEALTH_COMPONENT, ATTACK_COMPONENT};
+
+struct TransformComponent{
 	Vector2 position;
 	Vector2 velocity;
 };
+
+struct HealthComponent{
+	int healthPoints;
+	int maxHealth;
+	int regeneration;
+};
+
+//___________________________________________________________________________________________________________________________________________
 
 class ECS;
 
 using ComponentVariant = std::variant<TransformComponent>;
 
-class Entity{
+class Entity {
 public:
 	int id;
+	ECS* ecs;
 	std::vector<int> component_types;
 	std::vector<int> component_ids;
 
 	int GetId();
 	void SetId(int id);
 	std::vector<int> GetComponentTypes();
+	int GetComponentType(int index);
 	std::vector<int> GetComponentIds();
-
-    ECS* ecs;  // Pointer to ECS instance
+	int GetComponentId(int index);
     Entity(ECS* ecsInstance) : ecs(ecsInstance) {}
 
     template <typename T>
-    int AddComponent(int componentTypeId, T component);
-	void RemoveComponent(int ComponentTypeId);
+    int AddComponent(int componentTypeId, const T &component);
+	template <typename T>
+    bool SetComponent(int componentTypeId, const T& newComponentData);
+	void RemoveComponent(int componentTypeId);
+};
+
+class System {
+public:
+    virtual void Update(ECS& ecs) = 0; // Pure virtual function for updating components
 };
 
 class ECS {
 public:
 	std::vector<Entity> entities;
 	int entityId = 0;
-	std::vector<ComponentVariant> componentTypesArray;			//holds every component type
-	std::vector<TransformComponent> transformComponentsArray;	//holds the unique instances of one component type
 	std::unordered_map<int, std::vector<std::shared_ptr<void>>> componentArrays;	// Use an unordered_map to manage different component arrays
+	std::vector<std::shared_ptr<System>> systems;  									// List of systems
 
 	void Initialize();
 	Entity CreateEntity();
@@ -50,22 +69,13 @@ public:
 	void Update();
 	void Shutdown();
 
-    // Register component type arrays
     template <typename T>
     void RegisterComponentType(int componentTypeId);
+	void AddSystem(std::shared_ptr<System> system);
 };
 
-// Register a component type by ID
 template <typename T>
-void ECS::RegisterComponentType(int componentTypeId) {
-    if (componentArrays.find(componentTypeId) == componentArrays.end()) {
-        componentArrays[componentTypeId] = std::vector<std::shared_ptr<void>>();
-    }
-}
-
-// Add a component to an entity
-template <typename T>
-int Entity::AddComponent(int componentTypeId, T component) {
+int Entity::AddComponent(int componentTypeId, const T& component) {
     if (ecs->componentArrays.find(componentTypeId) != ecs->componentArrays.end()) {
         auto& componentArray = ecs->componentArrays[componentTypeId];
         int componentId = componentArray.size();
@@ -76,5 +86,74 @@ int Entity::AddComponent(int componentTypeId, T component) {
     }
     return -1; // Error if component type not found
 }
+
+// Set or update the data of a component for an entity
+template <typename T>
+bool Entity::SetComponent(int componentTypeId, const T& newComponentData) {
+    for (size_t i = 0; i < component_types.size(); ++i) {
+        if (component_types[i] == componentTypeId) {
+            int componentInstanceId = component_ids[i];
+            auto& componentArray = ecs->componentArrays[componentTypeId];
+            auto componentPtr = std::static_pointer_cast<T>(componentArray[componentInstanceId]);
+            if (componentPtr) {
+                *componentPtr = newComponentData;
+                return true;  // Successfully updated the component
+            }
+        }
+    }
+    return false;  // Component not found or update failed
+}
+
+// Register a component type by ID
+template <typename T>
+void ECS::RegisterComponentType(int componentTypeId) {
+    if (componentArrays.find(componentTypeId) == componentArrays.end()) {
+        componentArrays[componentTypeId] = std::vector<std::shared_ptr<void>>();
+    }
+}
+
+template <typename T>
+std::shared_ptr<T> GetComponent(Entity& entity, int componentTypeId) {
+    for (size_t i = 0; i < entity.component_types.size(); ++i) {
+        if (entity.component_types[i] == componentTypeId) {
+            int componentInstanceId = entity.component_ids[i];
+            auto& componentArray = entity.ecs->componentArrays[componentTypeId];
+            return std::static_pointer_cast<T>(componentArray[componentInstanceId]);
+        }
+    }
+    return nullptr; // Component not found
+}
+
+//_________________________________________________________________ SYSTEMS _________________________________________________________________
+
+class MovementSystem: public System {
+public:
+    void Update(ECS& ecs) override {
+        for (Entity& entity : ecs.entities) {
+            auto transformComponent = GetComponent<TransformComponent>(entity, TRANSFORM_COMPONENT);
+            if (transformComponent) {
+				transformComponent->position.x += transformComponent->velocity.x;
+				transformComponent->position.y += transformComponent->velocity.y;
+            }
+        }
+    }
+};
+
+class HealthSystem: public System {
+public:
+    void Update(ECS& ecs) override {
+        for (Entity& entity : ecs.entities) {
+            auto healthComponent = GetComponent<HealthComponent>(entity, HEALTH_COMPONENT);
+            if (healthComponent) {
+                if (healthComponent->maxHealth > healthComponent->healthPoints) {
+                    healthComponent->healthPoints += healthComponent->regeneration;
+                }
+                if (healthComponent->healthPoints > healthComponent->maxHealth) {
+                    healthComponent->healthPoints = healthComponent->maxHealth;
+                }
+            }
+        }
+    }
+};
 
 #endif // ECS_H
