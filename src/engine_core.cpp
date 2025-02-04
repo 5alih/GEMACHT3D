25d91383fb57a70ca5cdd9c2f0bef04805ab7d56
Salih <mustafahakan9215@gmail.com>
2025-02-04 13:08:02 +0300

    better grouping of elements

--- src/engine_core.cpp
+++ src/engine_core.cpp
@@ -1,7 +1,5 @@
 #include "engine_core.h"
 
-Camera3D playerCamera= {0};
-
 void CoreEngine::Initialize(){
 	InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "GEMACHT 3D");
 	SetWindowState(FLAG_WINDOW_RESIZABLE);
@@ -13,19 +11,8 @@ void CoreEngine::Initialize(){
 	ecs.Initialize();
 	developerConsole.Initialize();
 
-	playerCamera.position= {0.0f, 2.0f, 4.0f};
-	playerCamera.target= {0.0f, 2.0f, 0.0f};
-	playerCamera.up= {0.0f, 1.0f, 0.0f};
-	playerCamera.fovy= 60.0f;
-	playerCamera.projection= CAMERA_PERSPECTIVE;
-
-    auto drawSceneFunc= [](Camera3D& cam){
-		DrawGrid(20, 2.0f);
-		DrawCube( (Vector3){0.0f, 0.0f, 0.0f}, 1.0f, 1.0f, 1.0f, (Color){243, 169, 78, 255} );
-    };
-
     engine_font= LoadFontEx("resource/font/source-sans-pro.bold.ttf", 14, 0, 0);
-    swanGui= renderer.InitGui(engine_font, playerCamera, drawSceneFunc);
+    swanGui= renderer.InitGui(engine_font);
 }
 
 void CoreEngine::Run(){
