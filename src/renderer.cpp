2a2381a07b78b3cfc3997259ba119e8c5adbea1d
Salih <mustafahakan9215@gmail.com>
2025-02-04 12:21:13 +0300

    added dropdown menus

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,6 +9,10 @@ void Renderer::RenderPlayerView(Camera3D playerCamera){
 	EndMode3D();
 }
 
+// auto dd_settings= std::make_shared<DropDown>("Settings", 0, 3);
+// auto dd_scenes= std::make_shared<DropDown>("Scenes", 0, 3);
+// auto dd_themes= std::make_shared<DropDown>("Themes", 0, 3);
+
 void Renderer::RenderFPS(Color color){
 	DrawText(to_const_char(to_string(GetFPS())), GetMonitorWidth(0) -20, 5, 10, color);
 }
@@ -18,7 +22,7 @@ SwanGui Renderer::InitGui(Font font, Camera3D &camera, std::function<void(Camera
 
 	Vector2 p_settingsPos= {64, 19};
 	Vector2 p_settingsSize= {22	, 35};
-	auto p_settings= std::make_shared<Panel>("SETTINGS", p_settingsPos, p_settingsSize, 2, font);
+	auto p_settings= std::make_shared<Panel>("SETTINGS", p_settingsPos, p_settingsSize, 3, font);
 	p_settings->addElement(std::make_shared<Button>("Create entity", [](){}, false));
 	p_settings->addElement(std::make_shared<Button>("Delete entity", [](){}, false));
 	p_settings->addElement(std::make_shared<Button>("Save entities", [](){}, true));
@@ -57,11 +61,32 @@ SwanGui Renderer::InitGui(Font font, Camera3D &camera, std::function<void(Camera
 
 	Vector2 p_topPos= {0, 0};
 	Vector2 p_topSize= {96, 1};
-	auto p_top= std::make_shared<Panel>("p_top", p_topPos, p_topSize, false, 20, font);
+	auto p_top= std::make_shared<Panel>("p_top", p_topPos, p_topSize, false, 18, font);
 	p_top->addElement(std::make_shared<Comment>("GEMACHT 3D"));
-	p_top->addElement(std::make_shared<Button>("Setting", [](){}, false));
-	p_top->addElement(std::make_shared<Button>("Scenes", [](){}, false));
-	p_top->addElement(std::make_shared<Button>("Themes", [](){}, false));
+
+	auto dd_settings= std::make_shared<DropDown>("Settings", 0, 3);
+	auto dd_scenes= std::make_shared<DropDown>("Scenes", 0, 2);
+	auto dd_themes= std::make_shared<DropDown>("Themes", 0, 1);
+	p_top->addElement(dd_settings);
+	p_top->addElement(dd_scenes);
+	p_top->addElement(dd_themes);
+
+	static auto dd_p_settings= std::make_shared<Panel>("dd_p_settings", (Vector2){5.63, 1}, (Vector2){4.73, 3.55}, false, 1, font);
+	dd_p_settings->addElement(std::make_shared<Button>("Overwrite", [](){}, false));
+	dd_p_settings->addElement(std::make_shared<Button>("Reset", [](){}, false));
+	dd_p_settings->addElement(std::make_shared<Button>("Discard", [](){}, false));
+	dd_p_settings->addElement(std::make_shared<Button>("Save", [](){}, false));
+	dd_settings->SetPanel(dd_p_settings);
+
+	static auto dd_p_scenes= std::make_shared<Panel>("dd_p_scenes", (Vector2){10.967, 1}, (Vector2){4.73, 1.85}, false, 1, font);
+	dd_p_scenes->addElement(std::make_shared<Button>("Scene 1", [](){}, false));
+	dd_p_scenes->addElement(std::make_shared<Button>("Scene 2", [](){}, false));
+	dd_scenes->SetPanel(dd_p_scenes);
+
+	static auto dd_p_themes= std::make_shared<Panel>("dd_p_themes", (Vector2){16.3, 1}, (Vector2){4.73, 1}, false, 1, font);
+	dd_p_themes->addElement(std::make_shared<Button>("Dark Mode", [](){}, false));
+	dd_themes->SetPanel(dd_p_themes);
+
 	swanGui.AddPanel(p_top);
 
 	return swanGui;
@@ -75,5 +100,10 @@ void Renderer::RenderGui(SwanGui &swanGui, DeveloperConsole &developerConsole){
 	if(IsKeyPressed(KEY_ESCAPE) && developerConsole.GetIsEnabled()){
 		developerConsole.Exit();
 	}
+
+	// dd_settings->PrintDimensions();
+	// dd_scenes->PrintDimensions();
+	// dd_themes->PrintDimensions();
+
 	swanGui.Draw();
 }
